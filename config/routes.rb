c5c0d0520adb1a0f539f63976ab55f75ec24052a
“salomonrios” <salomonscaner@hotmail.com>
2014-08-06 13:35:21 -0500

    cambios rutas

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,13 @@ Rails.application.routes.draw do
   resources :proyects
 
   resources :proyects do
-  resources :engineers
+    resources :engineers
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'proyects#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
